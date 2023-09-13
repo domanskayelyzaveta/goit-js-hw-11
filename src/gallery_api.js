4b314d6dd17cb68f957acb14bc8fe5b9faab42dc
domanskayelyzaveta <domanskaya_@ukr.net>
2023-09-13 19:22:12 +0200

    push

--- src/gallery_api.js
+++ src/gallery_api.js
@@ -1,51 +1,43 @@
 import axios from "axios";
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from "notiflix/build/notiflix-notify-aio";import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+
+const galleryCard = new SimpleLightbox('.gallery a');
 // axios.defaults.headers.common["x-api-key"] = "39407568-0a070d3ffff032a43909d6f38";
 
 ////запити///
 
 export class PixabayApiFetch {
-  static baseUrl = "https://pixabay.com/api/";
-  static apiKey = "39407568-0a070d3ffff032a43909d6f38";
-  static url = `${PixabayApiFetch.baseUrl}?key=${PixabayApiFetch.apiKey}`;
+  static url = "https://pixabay.com/api/";
+
 
   constructor() {
     this.q = null;
     this.page = 1;
   }
 
-  fetchByTag(tag) {
-    const PARAMS = new URLSearchParams({
-      q: tag,
+  async fetchByTag() {
+      const PARAMS = new URLSearchParams({
+      key: "39407568-0a070d3ffff032a43909d6f38",
+      q: this.q,
       page: this.page,
-      per_page: 20,
+      per_page: 40,
       image_type: "photo",
       orientation: "horizontal",
       safesearch: true,
     });
+      
+      
+    const url = `${PixabayApiFetch.url}?${PARAMS}`;
 
-    const url = `${PixabayApiFetch.url}&${PARAMS}`;
-
-    return axios
-      .get(url)
-      .then((response) => response.data)
-      .catch((error) => {
-        Notify.failure(error);
-      });
+      try {
+          const response = await axios.get(url);
+          return response.data;
+      } catch (error) {
+          Notify.failure(error);
+      }
   }
 
-  fetchRandomPhotos() {
-    const axiosOptions = {
-      params: {
-        per_page: 20,
-        orientation: "horizontal",
-        key: PixabayApiFetch.apiKey,
-      },
-    };
-
-    return axios.get(`${PixabayApiFetch.baseUrl}/photos/random`, axiosOptions);
-  }
-}
-
+};
 
--- src/index.js
+++ src/index.js
@@ -1,37 +1,126 @@
 import { PixabayApiFetch } from "./gallery_api";
-
+import InfiniteScroll from 'infinite-scroll';
+import { Notify } from "notiflix";
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
-formEl: document.querySelector(".search-form"),
-}
-
-
-const pixabayApiFetch = new PixabayApiFetch();
-
-pixabayApiFetch.fetchByTag().then(data => { console.log(data);
-})
-.catch (error => {
-    console.log(error);
-});
-
-
-
-
-refs.formEl.addEventListener('submit', event => {
-    event.preventDefault();
-    pixabayApiFetch.fetchCatByTag().then(data => {
-        console.log(data);
-    })
-        .catch(error => {
-            console.log(error)
-        });
-});
-
-const createGallery = (photosArr) => {
-    const markUp = photosArr.map((photo) => {
-        return `<li class="gallery-item">
-        <img src="${photo.url}" alt="${photo.description}" />
-      </li>`;
-    })
+    formEl: document.querySelector(".search-form"),
+    galleryEl: document.querySelector(".gallery"),
+    loadBtn: document.querySelector(".load-btn"),
 };
+
+
+refs.loadBtn.classList.add("is-hidden");
+
+const lightbox = new SimpleLightbox('.gallery a');
+const pixabayApiFetch = new PixabayApiFetch();
+let page = 1;
+
+
+
+///////////////SCROLL////////////////
+
+// const infScroll = new InfiniteScroll(refs.galleryEl, {
+//   path: function () {
+//     return `/path-to-next-page?page=${page}`;
+//   },
+//   append: '.photo-card',
+//   checkLastPage: true, 
+//   history: false,
+//   button: refs.loadMoreButton,
+// });
+
+// infScroll.on('load', function () {
+//   page++;
+//   pixabayApiFetch.page = page;
+//   pixabayApiFetch.fetchByTag().then(data => {
+//     createGalleryCard(data.hits);
+//     infScroll.loadNextPage();
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+// });
+
+///////////////////////////////////////
+
+
+
+export const createGalleryCard = (photosArr) => {
+  const markUp = photosArr.map((photo) => {
+    return `
+   <div class="photo-card">
+        <a href="${photo.largeImageURL}" data-lightbox="gallery">
+          <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" class="photo-el" />
+        </a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes:</b> ${photo.likes}
+          </p>
+          <p class="info-item">
+            <b>Views:</b> ${photo.views}
+          </p>
+          <p class="info-item">
+            <b>Comments:</b> ${photo.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads:</b> ${photo.downloads}
+          </p>
+        </div>
+      </div>
+    `;
+  });
+    
+    refs.galleryEl.insertAdjacentHTML("beforeend", markUp.join(" ")); 
+    lightbox.refresh();
+};
+
+
+
+refs.formEl.addEventListener('submit', async event => {
+    event.preventDefault();
+    pixabayApiFetch.q = refs.formEl.elements.searchQuery.value.trim();
+   refs.loadBtn.classList.remove("is-hidden");
+
+
+    if (pixabayApiFetch.q === "") {
+        page = 1;
+        alert("Please write some text");
+        return;
+    }
+
+     
+    refs.galleryEl.innerHTML = "";
+    try {
+        const data = await pixabayApiFetch.fetchByTag()
+        if (data.totalHits < 40) {
+            refs.loadBtn.classList.add("is-hidden");
+        }
+
+
+        createGalleryCard(data.hits);
+        event.target.reset();
+    } catch(error) {
+        console.log(error);
+        }
+});
+
+
+
+refs.loadBtn.addEventListener('click', async event => {
+
+    try {
+        pixabayApiFetch.page += 1;
+        const data = await pixabayApiFetch.fetchByTag();
+        createGalleryCard(data.hits);
+
+        if (Math.ceil(data.totalHits/40) < pixabayApiFetch.page) {
+            refs.loadBtn.classList.add("is-hidden");
+            Notify.info("We're sorry, but you've reached the end of search results.");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
 
